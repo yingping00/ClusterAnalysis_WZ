06a639536aff636d6babb3c8c44201c930d8fb88
jasperwoo516@gmail.com <jasperwoo516@gmail.com>
2016-11-14 21:42:42 -0500

    finished read_graph.

--- K-means.c
+++ K-means.c
@@ -6,36 +6,49 @@
 #define MAX_ITERATIONS 100
 
 //read the txt file, construct and return a 2-d array
-int *[] read_data(FILE *data_file){
-	int num_lines,num_points;
-
-	while(!feof(data_file)){                  //count the number of data points in the file
-		char ch = fgetc(data_file);
-		if(ch == '\n'){
-			num_lines++;
-		}
-	}
-	num_points = num_lines-5;     
-	//Todo: count the number of data in a line to obtain the number of dimensions.
-	float mtrx[num_points][3];        //the 2-d array to store the data points
-	float (*mtrx_ptr)[3] = mtrx; 
+void read_data(FILE *data_file, int num_points, int num_col, float **mtrx){
 	for (int i=0; i<5; i++){           //skip the first 5 lines
 		fscanf(data_file, "%*[^\n]\n", NULL);
 	}
 
 	for (int i=0; i<num_points; i++){          //start reading data points to array. Point i stores in mtrx[i-1][];
-											   // mtrx[i-1][0] defines the cluster center it is assigned to; mtrx[i-1][1] is its first-dimension coordinate,and so forth.
-		fscanf(data_file,"%*f %f %f\n", &mtrx[i][0], &mtrx[i][1], &mtrx[i][2]);
+		fscanf(data_file,"%*f",	&mtrx[i][0]);   //mtrx[i-1][0] defines the cluster center it is assigned to; mtrx[i-1][1] is its first-dimension coordinate,and so forth.
+		mtrx[i][0]=0;					       //initialize the cluster center that each data point is assigned to as 0. 
+		for (int j=1;j<num_col; j++){
+			fscanf(data_file,"%f", &mtrx[i][j]);
+		}
+		fscanf(data_file,"\n",NULL);    
+		
 	}
-
-	for (int i=1;i<num_points;i++){
-		mtrx[i][0]=0;
-	}
-
-	return mtrx_ptr;
 } 
 
-int main(){
-	FILE *data_file = fopen("atlanta.txt","r");
-	read_data(data_file);
+//To run: a.exe 2 20 Atlanta.exe output.exe
+int main(int argc, char *argv[]){
+	//a guard
+	if(argc!= 5){
+		fprintf(stderr, "Must provide 4 args--an integer as number"
+				 "of the dimensions, another integer as number of points,"
+				 "a string specifying the filename of datasets, and another"
+				 " string for filename used to output resulut.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int num_dim = atoi(argv[1]), num_col = num_dim+1, num_points = atoi(argv[2]);
+	FILE *data_file = fopen(argv[3],"r");
+
+	float **mtrx = (float**)malloc(sizeof(float*)*num_points);
+	for (int i=0; i<num_points;i++){
+		mtrx[i] = (float*)malloc(sizeof(float)*num_col);
+	} 
+
+	read_data(data_file, num_points, num_col, mtrx);
+	
+	//Just for checking
+	for (int i=0; i<num_points; i++){
+		for (int j=0; j<num_col; j++){
+			printf("%f\t",mtrx[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }
